40df187af4612727f69a0f0b508107bdc26b1c7d
Π19028 <60019367+NikoletaUnipi@users.noreply.github.com>
2020-07-31 16:19:52 +0300

    Update inter.js

--- inter.js
+++ inter.js
@@ -1,55 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<title>Timer</title>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+<div id="container0">
+	<div id="counter">0</div>
+	<img src="tomato.png" id="tomatoIcon">
+</div>
+<div class="container">
 
+<p id="demo">25 : 00</p>
+<div id="buttons">
+<button id="startBtn">Start</button>
+<button id="stopBtn"> Stop</button>
 
-//instanciating variables
-var seconds = 1500;
-//state
-var state = false;
-//count the tomatos
-var counter = 0;
-//get elements from html
-document.getElementById("stopBtn").addEventListener('click',StopIt,false);
-document.getElementById("startBtn").addEventListener('click',Start,false);
-document.getElementById("restart").addEventListener('click',restart,false);
+</div>
+<div id="options">
+	<button id="tomato">Tomato</button>
+	<button id="shortBreak">Short Break</button>
+	<button id="longBreak">Long Break</button>
+</div>
+</div>
+<div class="container2">
+<p>This is a project that was made in order to help people with time management, using the <span id="pomodoro">pomodoro</span> technique.</p>
+<hr>
+</div>
+<script src="inter.js"></script>
+<script src="styling.js"></script>
+</body>
 
-
-//Counting_Down repeats every second
-var loop = setInterval(Counting_Down,1000);
-
-//functions
-
-//time loop throw a function
-function Counting_Down(){
-	if(state){	//if start if choosen
-	Show(seconds);//time gets updated
-	seconds--;//seconds reduced by 1
-		if(seconds<0){	//if time has ended
-			counter++;	//increase counter
-			document.getElementById("counter").innerHTML = counter;//update tomatos
-			StopIt();//stop counting
-		}
-	}
-}
-
-//show in doc
-function Show(sec){
-	var seconds = sec%60;
-	var minutes = Math.floor(sec/60);
-	document.getElementById("demo").innerHTML = minutes + " : " + seconds;
-	
-}
-
-//stops contdown
-function StopIt(){
-	state = false;
-}
-
-//start
-function Start(){
-	state = true;
-}
-
-//reset
-function restart(){
-	seconds = 1500;
-	Start();
-}
+</html>
